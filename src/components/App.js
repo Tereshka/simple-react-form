f98d66cfe0cf2f741ecc8e49e5d7cb758b4686d6
Tereshka <tereshka07@gmail.com>
2019-10-27 19:50:02 +0900

    onchange input, add select

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,32 @@
 import React from "react";
+import countries from "../data/countries";
 
 export default class App extends React.Component {
+
+  state = {
+    username: '',
+    password: '',
+    passwordRepeat: '',
+    country: '',
+  }
+
+  onChangeInput = (event) => {
+    this.setState({[event.target.name]: event.target.value});
+  }
+
+  submit = (event) => {
+    event.preventDefault();
+    console.log(this.username.value, this.password.value);
+  }
+
+  renderItemsOptions = (items) => {
+    return items.map((country, index) => (
+      <option key={index} value={country.id}>{country.name}</option>
+    ));
+  }
+
   render() {
+    const {username, password, passwordRepeat} = this.state;
     return (
       <div className="form-container card">
         <form className="form card-body">
@@ -11,6 +36,10 @@ export default class App extends React.Component {
               type="text"
               className="form-control"
               placeholder="Enter username"
+              ref={node => (this.username = node)}
+              name="username"
+              value={username}
+              onChange={ this.onChangeInput }
             />
           </div>
           <div className="form-group">
@@ -19,6 +48,10 @@ export default class App extends React.Component {
               type="text"
               className="form-control"
               placeholder="Enter password"
+              ref={node => (this.password = node)}
+              name="password"
+              value={password}
+              onChange={this.onChangeInput}
             />
           </div>
           <div className="form-group">
@@ -27,9 +60,25 @@ export default class App extends React.Component {
               type="text"
               className="form-control"
               placeholder="Enter repeat password"
+              ref={node => (this.passwordRepeat = node)}
+              name="passwordRepeat"
+              value={passwordRepeat}
+              onChange={this.onChangeInput}
             />
           </div>
-          <button type="submit" className="btn btn-primary w-100">
+          <div className="form-group">
+            <label htmlFor="country">Country</label>
+            <select
+              className="form-control"
+              id="country"
+              name="country"
+              value={this.state.country}
+              onChange={this.onChangeInput}
+            >
+              {this.renderItemsOptions(countries)}
+            </select>
+          </div>
+          <button type="submit" className="btn btn-primary w-100" onClick={this.submit}>
             Submit
           </button>
         </form>
