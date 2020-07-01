e69e9213539d5bb7ee72be2ced18b51c7efd1418
Tereshka <tereshka07@gmail.com>
2020-07-01 15:24:37 +0900

    Updates

---
+++ precache-manifest.7909c04f5e5f608b6948b37244481da4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3e8b74b804fcd5b112e",
+    "url": "/simple-react-form/static/js/runtime~main.a3e8b74b.js"
+  },
+  {
+    "revision": "f5551b0d495bf4b67674",
+    "url": "/simple-react-form/static/js/main.f5551b0d.chunk.js"
+  },
+  {
+    "revision": "cae9515c52335bfb8b66",
+    "url": "/simple-react-form/static/js/2.cae9515c.chunk.js"
+  },
+  {
+    "revision": "f5551b0d495bf4b67674",
+    "url": "/simple-react-form/static/css/main.60988682.chunk.css"
+  },
+  {
+    "revision": "cae9515c52335bfb8b66",
+    "url": "/simple-react-form/static/css/2.66e5a27c.chunk.css"
+  },
+  {
+    "revision": "c37adf795d1a6eb9e998239cda0db334",
+    "url": "/simple-react-form/index.html"
+  }
+];
